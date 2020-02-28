b8f048f671d4d0191dc79a1974eee24e2acc2313
arijamarta <58549408+arijamarta@users.noreply.github.com>
2020-02-28 14:30:32 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ $(document).ready(function() {
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "none");
 
-    $("#header").css("background", "black");
+    $("#header").css("background", "pink");
     $("#header").css("width", "100%");
     $("#header").css("height", "20%");
 
-    $("#body").css("background", "blue");
+    $("#body").css("background", "white");
     $("#body").css("width", "100%");
     $("#body").css("height", "75%");
     
     $("#footer").css("background", "red");
     $("#footer").css("width", "100%");
-    $("#footer").css("height", "5%");
+    $("#footer").css("height", "20%");
 
     $("#navigation_header").css("padding-top", "20px");
     $("#navigation_header").css("padding-bottom", "20px");
 
-    $("#body_content").css("background", "yellow");
+    $("#body_content").css("background", "white");
 
     $("#personal_photo").css("height", "250px");
     $("#personal_photo").css("width", "250px");
